1e883681c0dac07c97c08ec6755aa7ebe5e95fb5
Faran Ali <faranaliahmed@gmail.com>
2026-01-24 17:01:48 +0500

    feat: Implement form request generation
    - Add RequestGenerator extending base Generator
    - Generate requests in Services/{Service}/Modules/{Module}/Requests/
    - Auto-append 'Request' suffix to class names
    - Validate service existence before generation
    - Detect duplicate requests
    - Add MakeRequestCommand with minimal output
    - Arguments: name, module, service

---
+++ src/Generators/RequestGenerator.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Faran\Pulse\Generators;
+
+use Exception;
+use Faran\Pulse\Exceptions\ServiceDoesNotExistException;
+
+class RequestGenerator extends Generator
+{
+    /**
+     * The name of the request to generate.
+     */
+    protected string $name;
+
+    /**
+     * The name of the module.
+     */
+    protected string $module;
+
+    /**
+     * The name of the service.
+     */
+    protected string $service;
+
+    /**
+     * Create a new RequestGenerator instance.
+     *
+     * @param  string  $name
+     * @param  string  $module
+     * @param  string  $service
+     */
+    public function __construct(string $name, string $module, string $service)
+    {
+        $this->name = $this->ensureRequestSuffix($name);
+        $this->module = $module;
+        $this->service = $service;
+    }
+
+    /**
+     * Generate the request file.
+     *
+     * @throws Exception
+     */
+    public function generate(): string
+    {
+        $this->validateServiceExists();
+        $this->createModuleDirectories();
+
+        $filePath = $this->getRequestPath();
+
+        if ($this->fileExists($filePath)) {
+            throw new Exception("Request [{$this->name}] already exists in {$this->service}/{$this->module}!");
+        }
+
+        $content = $this->getRequestContent();
+        $this->createFile($filePath, $content);
+
+        return $this->getRelativePath($filePath);
+    }
+
+    /**
+     * Validate that the service exists.
+     *
+     * @throws ServiceDoesNotExistException
+     */
+    protected function validateServiceExists(): void
+    {
+        if (!$this->serviceExists($this->service)) {
+            throw ServiceDoesNotExistException::make($this->service);
+        }
+    }
+
+    /**
+     * Create module directories if they don't exist.
+     */
+    protected function createModuleDirectories(): void
+    {
+        $modulePath = $this->getModulePath();
+        $requestsPath = $modulePath . DIRECTORY_SEPARATOR . 'Requests';
+
+        $this->createDirectory($modulePath);
+        $this->createDirectory($requestsPath);
+    }
+
+    /**
+     * Get the request file path.
+     */
+    protected function getRequestPath(): string
+    {
+        return $this->getModulePath() . DIRECTORY_SEPARATOR . 'Requests' . DIRECTORY_SEPARATOR . $this->name . '.php';
+    }
+
+    /**
+     * Get the module path.
+     */
+    protected function getModulePath(): string
+    {
+        return $this->findServicesRootPath() . DIRECTORY_SEPARATOR . $this->service . DIRECTORY_SEPARATOR . 'Modules' . DIRECTORY_SEPARATOR . $this->module;
+    }
+
+    /**
+     * Get the request content from stub.
+     *
+     * @return string
+     * @throws Exception
+     */
+    protected function getRequestContent(): string
+    {
+        $stubPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . 'request.stub';
+
+        if ($this->fileExists($stubPath)) {
+            $stub = $this->loadStub($stubPath);
+            return $this->replaceStubPlaceholders($stub, [
+                'namespace' => $this->getNamespace(),
+                'name' => $this->name,
+                'service' => $this->service,
+                'module' => $this->module,
+            ]);
+        }
+
+        throw new Exception('Request stub not found');
+    }
+
+    /**
+     * Get the request namespace.
+     *
+     * @return string
+     * @throws Exception
+     */
+    protected function getNamespace(): string
+    {
+        $rootNamespace = $this->findRootNamespace();
+        return "{$rootNamespace}\\Services\\{$this->service}\\Modules\\{$this->module}\\Requests";
+    }
+
+    /**
+     * Ensure the request name has the "Request" suffix.
+     */
+    protected function ensureRequestSuffix(string $name): string
+    {
+        if (!str_ends_with($name, 'Request')) {
+            return $name . 'Request';
+        }
+
+        return $name;
+    }
+
+    /**
+     * Get the relative path from the Laravel root.
+     */
+    protected function getRelativePath(string $fullPath): string
+    {
+        $laravelRoot = $this->findLaravelRoot();
+        return str_replace($laravelRoot . DIRECTORY_SEPARATOR, '', $fullPath);
+    }
+}
